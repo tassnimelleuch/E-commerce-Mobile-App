a87c847b240c52869ea946a6ef8b587485e21356
tassnimelleuch <tasnim.eleuch@enis.tn>
2024-05-05 17:37:58 +0000

    finished project

---
+++ app/src/main/java/com/example/afinal/RegistrationActivity.java
@@ -0,0 +1,23 @@
+package com.example.afinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+    public void signup (View view){
+        startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+
+    }
+    public void signin (View view){
+        startActivity(new Intent(RegistrationActivity.this,LOGINActivity.class));
+
+}}
